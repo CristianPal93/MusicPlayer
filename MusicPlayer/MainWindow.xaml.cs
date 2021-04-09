939dc264416cc287c77761d12d63d9554a8c5d1f
Cristian Pal <cristian.pal93@e-uvt.ro>
2021-04-09 08:49:34 +0300

    ui start

--- MusicPlayer/MainWindow.xaml.cs
+++ MusicPlayer/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MaterialDesignThemes.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,11 +19,84 @@ namespace MusicPlayer
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+   
     public partial class MainWindow : Window
     {
+        private bool isPlaying = false;
         public MainWindow()
         {
-            InitializeComponent();
+            try
+            {
+                InitializeComponent();
+            }
+            catch (System.Windows.Markup.XamlParseException e) {
+
+                Console.WriteLine(e);
+            }
         }
+
+        private void CloseApp(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+
+        }
+
+        private void WindowMinimized(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+            
+        }
+
+        private void WindowMaximized(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            PackIcon pack = (button.FindName("WindowSettings") as PackIcon);
+            if (pack.Kind.Equals(PackIconKind.WindowMaximize))
+            {
+                this.WindowState = WindowState.Maximized;
+                (button.FindName("WindowSettings") as PackIcon).Kind = PackIconKind.DockWindow;
+
+
+            }
+            else {
+                this.WindowState = WindowState.Normal;
+                (button.FindName("WindowSettings") as PackIcon).Kind = PackIconKind.WindowMaximize;
+
+
+            }
+            Console.WriteLine(pack.Kind);
+            
+        }
+
+        private void Anterior_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Urmator_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PlayPause_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            PackIcon pack = (button.FindName("PlayPauseIcon") as PackIcon);
+            if (pack.Kind.Equals(PackIconKind.Play))
+            {
+                this.isPlaying = true;
+                (button.FindName("PlayPauseIcon") as PackIcon).Kind = PackIconKind.Pause;
+
+            }
+            else {
+
+                this.isPlaying = false;
+                (button.FindName("PlayPauseIcon") as PackIcon).Kind = PackIconKind.Play;
+
+            }
+
+
+        }
+
     }
 }
